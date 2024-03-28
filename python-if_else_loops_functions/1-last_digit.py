d22e1666aaeaa76abc6f5417d88af7ccbfd1c8fe
CedricChauvet <chauvetc.ujf@gmail.com>
2024-03-28 16:05:21 +0100

    keep cool

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,16 @@
 import numpy as np
 import random
 number = random.randint(-10000, 10000)
-last_number =np.sign(number) * int(str(number)[-1])
-if last_number > 5:
-    than = "and is greater than 5"
-    print("Last digit of", number, "is", last_number, than)
-elif last_number ==0:
-    print("Last digit of", number, "is", last_number, "and is 0")
-elif last_number < 6:
-    than = "and is less than 6 and not 0"
-    print("Last digit of", number, "is", last_number, than)
+try:
+    last_number =np.sign(number) * int(str(number)[-1])
+    if last_number > 5:
+        than = "and is greater than 5"
+        print("Last digit of", number, "is", last_number, than)
+    elif last_number ==0:
+        print("Last digit of", number, "is", last_number, "and is 0")
+    elif last_number < 6:
+        than = "and is less than 6 and not 0"
+        print("Last digit of", number, "is", last_number, than)
+
+except ValueError as err:
+    return "TypeError"
