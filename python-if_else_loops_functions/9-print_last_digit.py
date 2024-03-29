8a7d7a7f640ba2ef83c00efb7cf421bd05ef7edf
CedricChauvet <chauvetc.ujf@gmail.com>
2024-03-29 14:33:25 +0100

    clouds

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if type(number) is int:     
-        last_digit= str(number)[-1]
+    if type(number) is int:
+        last_digit = str(number)[-1]
         print("{}".format(last_digit), end="")
         return last_digit
     else:
         raise Exception('patati patata')
-        
