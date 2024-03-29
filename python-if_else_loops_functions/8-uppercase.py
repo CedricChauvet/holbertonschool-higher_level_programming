f9661d27ec96a5f44c1753bcf2b0e241c1eac322
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-03-29 12:38:36 +0100

    a valider

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-def uppercase(Fring:str):
-    
+def uppercase(Fring: str):
     for u in Fring:
         if ord(u) < 123 and ord(u) >= 97:
             k = (ord(u)-32)
         else:
             k = ord(u)
-           
         print("{}".format(chr(k)), end='')
     print()
