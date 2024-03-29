81f58751e4d16a0aec8810dd3c72c9a1a28493d0
CedricChauvet <chauvetc.ujf@gmail.com>
2024-03-29 14:23:59 +0100

    clouds

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     last_digit= str(number)[-1]
-    print("{}".format(last_digit))
-
-print_last_digit(98)    
+    print("{}".format(last_digit), end="")
