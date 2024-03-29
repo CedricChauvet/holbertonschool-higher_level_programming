710ca5d5fa93589f731735f163f7a6f9205f2056
CedricChauvet <chauvetc.ujf@gmail.com>
2024-03-29 15:56:33 +0100

    clouds

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" main qui est executable
+"""
 Square = __import__('0-square').Square
 my_square = Square()
 print(type(my_square))
--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" descriptoin du module Saquare
+"""
+
 class Square:
     """ description de la classe Square """
     pass
