aa42ac388afc8045227b59e23b2272d763903bc0
CedricChauvet <chauvetc.ujf@gmail.com>
2024-03-29 16:44:18 +0100

    Square 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+descrition de  du module
+"""
+
+
+class Square:
+    """ Classe Square """
+
+    def __init__(self, f=0):
+        self.__size = self.ma_size(f)
+
+    def ma_size(self, f):
+        if type(f) is not int:
+            raise TypeError("size must be an integer")
+        elif f < 0:
+            raise ValueError("size must be >= 0")
+        elif type(f) is int:
+            return f
