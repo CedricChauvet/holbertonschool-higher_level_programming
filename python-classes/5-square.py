72bed5b53ddb61067f1fc845aa1ee3b473db04bd
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-03-30 11:49:33 +0100

    sat commit

---
+++ python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+descrition de  du module
+"""
+
+
+class Square:
+    """ Classe Square """
+
+    def __init__(self, f=0):
+        if type(f) is not int:
+            raise TypeError("size must be an integer")
+        elif f < 0:
+            raise ValueError("size must be >= 0")
+        elif type(f) is int:
+            self.__size = f
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, f):
+        if type(f) is not int:
+            raise TypeError("size must be an integer")
+        elif f < 0:
+            raise ValueError("size must be >= 0")
+        elif type(f) is int:
+            self.__size = f
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
