09a997aaa2561fd4a64a2ff681eeaa1b67293dbf
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-03-30 12:48:19 +0100

    saturday

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -8,7 +8,9 @@ class Square:
     """ Classe Square """
 
     def __init__(self, f=0, position=(0, 0)):
-        if position[0] < 0 or position[1] < 0:
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.position = position
