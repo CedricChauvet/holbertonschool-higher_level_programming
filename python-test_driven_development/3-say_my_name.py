a62c7ba3b82665c4e9ed585cd32f2bd43c48e4ff
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-01 17:33:59 +0200

    easter monday

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -9,4 +9,7 @@ def say_my_name(first_name,last_name=""):
     
     if type(first_name) != str:
         raise Exception("first_name must be a string")
+        
+    if type(last_name) != str:
+        raise Exception("last_name must be a string")
     print("My name is", first_name, last_name)
