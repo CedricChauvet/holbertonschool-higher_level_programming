aba447c06d0816cdad78cc24a45419c7f1266fa0
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-01 17:53:39 +0200

    easter monday

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,10 +9,15 @@ def print_square(size):
     fonction qui ecrit des #
     """
 
+    if size <0:
+         raise ValueError("size must be >= 0")
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    
+
     for i in range(size):
         print("")
         for j in range(size):
             print("#", end="")
     
-    if size <0:
-         raise Exception("size must be >= 0")
+    
