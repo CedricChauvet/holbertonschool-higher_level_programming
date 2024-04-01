8e6c0fc7c90a89f6be783cc2a4c84dadfd1f9931
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-01 17:04:42 +0200

    easter monday

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -15,24 +15,26 @@ def matrix_divided(matrix, div):
     """
     indexcol = len(matrix)
     indexrow = len(matrix[0])
-    
-    #check le diviseur est un scalaire
+
+    # check le diviseur est un scalaire
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
-    
+
     for i in range(indexcol):
         if len(matrix[i]) == len(matrix[0]):
             pass
         else:
-            raise TypeError("Each row of the matrix must have the same size") 
+            raise TypeError("Each row of the matrix must have" + \
+            " the same size")
     matrixdiv = []
     for i in range(indexcol):
         matrixdiv.append([])
         for j in range(indexrow):
             # check si tout les elements sont des int ou des floats
             if type(matrix[i][j]) not in [int, float]:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix " + \
+                "(list of lists) of integers/floats")
             # effectue la division
             matrixdiv[i].append(round(matrix[i][j]/div, 2))
-    
+
     return matrixdiv
