a99dbaa2385e8e82e57b546387b3c5504818af48
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-01 17:41:17 +0200

    easter monday

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,12 +4,12 @@ descrition du module say my name
 """
 
 
-def say_my_name(first_name,last_name=""):
+def say_my_name(first_name, last_name=""):
     """ Affiche une ligne de texte"""
-    
-    if type(first_name) != str:
+
+    if type(first_name) is not str:
         raise Exception("first_name must be a string")
-        
-    if type(last_name) != str:
+
+    if type(last_name) is not str:
         raise Exception("last_name must be a string")
     print("My name is", first_name, last_name)
