122f0c6e0a5dcd6561d1a7bcd63fc1c271ab3367
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-07 17:57:26 +0200

    plus qu'une heure

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,18 +1,6 @@
 #!/usr/bin/node
-// Import the built-in 'fs' module for file system operations
-const fs = require('fs');
-const { stdout } = require('process');
-
-// Check if a file path argument is provided
-if (process.argv.length < 3) {
-    console.error('Usage: node readFile.js <file-path>');
-    process.exit(1);
-}
-
 // Get the file path from the command line arguments
 const URL = process.argv[2];
-
-
 
 const request = require('request');
 request(URL, function (error, response, body) {
