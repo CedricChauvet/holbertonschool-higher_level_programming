ae515c6477ac854905125ca0d1634fe62f5e4b02
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-07 16:36:01 +0200

    plus qu'une heure

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+// Import the built-in 'fs' module for file system operations
+const fs = require('fs');
+const { stdout } = require('process');
+
+// Check if a file path argument is provided
+if (process.argv.length < 3) {
+    console.error('Usage: node readFile.js <file-path>');
+    process.exit(1);
+}
+
+// Get the file path from the command line arguments
+const filePath = process.argv[2];
+
+// Read the file asynchronously
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err.message);
+    } else {
+       
+    process.stdout.write(data);
+    }
+});
